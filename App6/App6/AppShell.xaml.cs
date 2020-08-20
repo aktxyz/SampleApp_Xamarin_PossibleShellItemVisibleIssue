c6a40cf18a806ac92a32d6591d235a4eb5de42df
aktxyz <aktxyz@gmail.com>
2020-08-20 13:55:58 -0500

    Add project files.

---
+++ App6/App6/AppShell.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+using App6.Views;
+
+namespace App6
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public static AppShell Instance { get; set; }
+
+        public AppShell()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            Instance = this;
+        }
+
+        private int _mode = 1;
+
+        public int Mode
+        {
+            get { return _mode; }
+            set
+            {
+                if (_mode != value)
+                {
+                    _mode = value;
+                    OnPropertyChanged(nameof(Mode));
+                    OnPropertyChanged(nameof(IsMode1));
+                    OnPropertyChanged(nameof(IsMode2));
+                }
+            }
+        }
+
+        public bool IsMode1 { get { return _mode == 1; } }
+        public bool IsMode2 { get { return _mode == 2; } }
+
+    }
+}
---
+++ App6/App6/Views/AboutPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App6.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private string _text1 = "";
+        public string Text1
+        {
+            get { return _text1; }
+            set
+            {
+                if (_text1 != value)
+                {
+                    _text1 = value;
+                    OnPropertyChanged(nameof(Text1));
+                }
+            }
+        }
+
+        private string _text2 = "";
+        public string Text2
+        {
+            get { return _text2; }
+            set
+            {
+                if (_text2 != value)
+                {
+                    _text2 = value;
+                    OnPropertyChanged(nameof(Text2));
+                }
+            }
+        }
+
+        public void OnTapMode0(object sender, EventArgs args) { AppShell.Instance.Mode = 0; Text1 = "About/LastClicked/000"; Text2 = "About/LastClicked/000"; }
+        public void OnTapMode1(object sender, EventArgs args) { AppShell.Instance.Mode = 1; Text1 = "About/LastClicked/111"; Text2 = "About/LastClicked/111"; }
+        public void OnTapMode2(object sender, EventArgs args) { AppShell.Instance.Mode = 2; Text1 = "About/LastClicked/222"; Text2 = "About/LastClicked/222"; }
+
+    }
+}
